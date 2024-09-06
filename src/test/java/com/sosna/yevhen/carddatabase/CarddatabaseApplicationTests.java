f78903acebe9b9ebb57e5e1cd7dcaa99929d0085
Evhen Sosna <eugensosna@gmx.de>
2024-09-06 15:22:55 +0200

    init spring

---
+++ src/test/java/com/sosna/yevhen/carddatabase/CarddatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosna.yevhen.carddatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarddatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
