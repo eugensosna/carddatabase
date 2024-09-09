07a4e3e8080865431cb8b4af732780258da743fc
Evhen Sosna <eugensosna@gmx.de>
2024-09-09 10:43:17 +0200

    fix bug

--- src/main/java/com/sosna/yevhen/carddatabase/domain/CarRepository.java
+++ src/main/java/com/sosna/yevhen/carddatabase/domain/CarRepository.java
@@ -1,5 +1,7 @@
 package com.sosna.yevhen.carddatabase.domain;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
@@ -16,4 +18,20 @@ public interface CarRepository extends CrudRepository<Car, Long>{
 	List<Car> findByColor(String color);
 	// Fetch cars by model year
 	List<Car> findByModelYear(int modelYear);
+	// Filter by brand and model
+	
+	List<Car> findByBrandAndModel(String brand, String model);
+	// Fetch cars by brand or color
+	List<Car> findByBrandOrColor(String brand, String color);
+	
+	// Fetch cars by brand using SQL
+	@Query("select c from Car c where c.model = ?1")
+	List<Car> findByModel(String model);
+	
+	
+	
+	
 }
+
+
+
