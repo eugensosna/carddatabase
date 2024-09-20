e4b6b2c44c799327d48d876eef92590e565383c4
Yevhen Sosna <yevhensosna@gmx.de>
2024-09-20 09:48:38 +0200

    fix merge erors
    Signed-off-by: Yevhen Sosna <yevhensosna@gmx.de>

--- src/main/java/com/sosna/yevhen/carddatabase/CarddatabaseApplication.java
+++ src/main/java/com/sosna/yevhen/carddatabase/CarddatabaseApplication.java
@@ -21,9 +21,7 @@ public class CarddatabaseApplication implements CommandLineRunner {
 	private final CarRepository repository;
 	private final OwnerRepository orepository;
 
-
-	public CarddatabaseApplication(CarRepository repository, 
-			OwnerRepository orepository) {
+	public CarddatabaseApplication(CarRepository repository, OwnerRepository orepository) {
 		this.repository = repository;
 		this.orepository = orepository;
 	}
@@ -44,16 +42,20 @@ public class CarddatabaseApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		repository.save(new Car("Ford", "Mustang", "Redd", "Ob-1027", 2015, 5900));
-		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000));
-		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000));
-		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000));
-		
-		//Fetch all cars
-		for(Car car: repository.findAll()) {
-			logger.info("brand:{} , model :{}",car.getBrand(), car.getModel());
-			
+
+		if (repository.findByModel("Mustang").size() <= 0) {
+			Owner owner1 = new Owner("Mary", "Robinson");
+
+			repository.save(new Car("Ford", "Mustang", "Redd", "Ob-1027", 2015, 5900, owner1));
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner1));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner1));
+		}
+
+		// Fetch all cars
+		for (Car car : repository.findAll()) {
+			logger.info("brand:{} , model :{}", car.getBrand(), car.getModel());
+
 		}
 	}
 
